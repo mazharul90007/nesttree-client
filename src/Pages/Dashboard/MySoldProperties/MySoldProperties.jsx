9b0ac78c82a73f8b6934e2259ccc4b94ecf44191
mazharul90007 <mazharul90007@gmail.com>
2025-01-26 23:23:57 +0600

    total sold amount of a specific agent has been shown in my sold properties section of agent dashboard

--- src/Pages/Dashboard/MySoldProperties/MySoldProperties.jsx
+++ src/Pages/Dashboard/MySoldProperties/MySoldProperties.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/useAuth";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { BiDollar } from "react-icons/bi";
 
 
 const MySoldProperties = () => {
@@ -8,11 +9,11 @@ const MySoldProperties = () => {
     const axiosSecure = useAxiosSecure();
     const { user } = useAuth();
 
-    const { data: soldProperties = [] } = useQuery({
+    const { data: soldProperties = {properties: [], totalSell: 0} } = useQuery({
         queryKey: ['soldProperties'],
         queryFn: async () => {
             const res = await axiosSecure.get(`/soldProperties/${user.email}`)
-            console.log(res.data)
+            // console.log(res.data)
             return res.data;
         }
     });
@@ -24,9 +25,16 @@ const MySoldProperties = () => {
 
     return (
         <div>
-            <div className=" border border-amber-600 p-4 w-fit flex gap-3 rounded mx-auto my-10">
-                <h2 className="text-4xl border-r-2 border-amber-600 pr-3">Sold Properties</h2>
-                <p className="text-4xl font-bold text-amber-700">{soldProperties.length}</p>
+            <div className="flex gap-4 items-center justify-center">
+                <div className=" border border-amber-600 p-4 w-fit flex gap-3 rounded  my-10">
+                    <h2 className="text-4xl border-r-2 border-amber-600 pr-3">Sold Properties</h2>
+                    <p className="text-4xl font-bold text-amber-700">{soldProperties.properties.length}</p>
+                </div>
+
+                <div className=" border border-amber-600 p-4 w-fit flex gap-3 rounded my-10">
+                    <h2 className="text-4xl border-r-2 border-amber-600 pr-3">Total Sell </h2>
+                    <p className="text-4xl font-bold text-amber-700 flex items-center"><BiDollar />{soldProperties.totalSell.toLocaleString()}</p>
+                </div>
             </div>
             <div className="overflow-x-auto">
                 <table className="table table-zebra w-full">
@@ -41,14 +49,14 @@ const MySoldProperties = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {soldProperties.length == 0 ? (
+                        {soldProperties.properties.length == 0 ? (
                             <tr>
                                 <td colSpan="7" className="text-center">
                                     No properties available.
                                 </td>
                             </tr>
                         ) : (
-                            soldProperties.map((property, index) => (
+                            soldProperties.properties.map((property, index) => (
                                 <tr key={property._id}>
                                     <th>{index + 1}</th>
                                     <td>{handleEmptyField(property.propertyTitle)}</td>
